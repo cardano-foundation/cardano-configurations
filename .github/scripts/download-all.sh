90f188c5f1f9ee4e35344e76f9381d2e31ca5f3d
KtorZ <matthias.benkort@gmail.com>
2025-03-18 15:02:09 +0100

    Port and clean-up scripts over from input-output-hk/cardano-configurations.

---
+++ .github/scripts/download-all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Download configuration files from a nominated URL for a nominated network
+
+CARDANO_CONFIG_URL=$1
+CARDANO_NETWORK=$2
+
+mkdir -p \
+  network/$CARDANO_NETWORK/cardano-node \
+  network/$CARDANO_NETWORK/genesis \
+  network/$CARDANO_NETWORK/cardano-db-sync \
+  network/$CARDANO_NETWORK/cardano-submit-api
+
+# Configuration files
+wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK/topology.json -O network/$CARDANO_NETWORK/cardano-node/topology.json
+wget -qO- $CARDANO_CONFIG_URL/$CARDANO_NETWORK/config.json \
+  | jq '.ByronGenesisFile = "../genesis/byron.json" | .ShelleyGenesisFile = "../genesis/shelley.json" | .AlonzoGenesisFile = "../genesis/alonzo.json" | .ConwayGenesisFile = "../genesis/conway.json"' \
+  | jq '.' > network/$CARDANO_NETWORK/cardano-node/config.json
+wget -qO- $CARDANO_CONFIG_URL/$CARDANO_NETWORK/db-sync-config.json \
+  | jq '.NodeConfigFile = "../cardano-node/config.json"' \
+  | jq '.' > network/$CARDANO_NETWORK/cardano-db-sync/config.json
+wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK/submit-api-config.json -O network/$CARDANO_NETWORK/cardano-submit-api/config.json
+
+# Genesis
+wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK/byron-genesis.json -O network/$CARDANO_NETWORK/genesis/byron.json
+wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK/shelley-genesis.json -O network/$CARDANO_NETWORK/genesis/shelley.json
+wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK/alonzo-genesis.json -O network/$CARDANO_NETWORK/genesis/alonzo.json
+wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK/conway-genesis.json -O network/$CARDANO_NETWORK/genesis/conway.json
